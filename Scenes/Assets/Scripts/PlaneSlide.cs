fe2cb431afec3f8c706bce40701d01815c77c6f3
Bobby Fata <bobbyfata@gmail.com>
2015-01-27 13:06:29 -0500

    Created Test Scene 1
    2 semi-transparent planes sliding over one another using iTween

---
+++ Scenes/Assets/Scripts/PlaneSlide.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneSlide : MonoBehaviour {
+	public float moveDist;
+
+	// Use this for initialization
+	void Start () {
+			iTween.MoveBy(gameObject, iTween.Hash("x", moveDist, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+		}
+	}
